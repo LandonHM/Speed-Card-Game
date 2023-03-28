5f7cb7cf78d90b9ae6e335461ba8542a606abdbc
LandonHM <pureskillezsandwitch@gmail.com>
2023-03-28 13:27:53 -0500

    Moved form into own file and added host option

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,51 @@
 <script lang="ts">
+  import Form from './Form.svelte';
   import type { ActionData, PageData } from "./$types";
   export let data: PageData;
   export let form: ActionData;
-  console.log(form);
+  //console.log(form);
   //console.log(data);
+  let host = data.host === 'true';
 </script>
 
-<form method="POST">
-  <div class="row">
-  <label>
-    Username
-    <input name="username" type="text">
-  </label>
-  </div>
-  <div class="row">
-  <label>
-    Lobby name
-    <input name="lobbyname" type="text">
-  </label>
-  </div>
-  <div class="row">
-  <label>
-    Password
-    <input name="password" type="password">
-  </label>
-
-  </div>
-<div class="row">
-    <button>Log in</button>
+<div class='box'>
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <div class='row'>
+    <div class='host' on:click={(e) => {host = true}}>
+      <p>Host</p>
     </div>
-</form>
-
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class='connect' on:click={(e) => {host = false}}>
+      <p>Connect</p>
+    </div>
+  </div>
+  <div class='row'>
+    {#if host}
+    <Form action="?/host" class="host"/>
+    {:else}
+    <Form action="?/connect" class="connect"/>
+    {/if}
+  </div>
+</div>
 {#if !form?.success && data.lobbyname}
 <p>{data.lobbyname} not found.</p>
 {/if}
+
+<style>
+  .box {
+    display: inline;
+    background-color: aqua;
+    width: auto;
+  }
+  .host {
+    border-color: black;
+    background-color: green;
+    border-style: solid;
+  }
+
+  .connect {
+    border-color: black;
+    background-color: tan;
+    border-style: solid;
+  }
+</style>
